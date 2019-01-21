99a92eac916bb58f49c0a3de90aed39efe7ac735
Walter Junior <juniordeonisio@gmail.com>
2019-01-21 19:37:42 -0200

    Initial Commit

---
+++ PagTed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PagTed
+//
+//  Created by WalterJunior on 1/21/19.
+//  Copyright © 2019 PagTeste. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
